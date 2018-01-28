c61c8b5e75d12fafdd2801d85d60e619f4cd9094
xon-xoff <dantesraven@yahoo.com>
2018-01-28 16:36:43 -0500

    Initial commit, Jan 28 2018

---
+++ caesar_cipher.rb
@@ -0,0 +1,12 @@
+### Function: caesar_cipher ###
+# Takes two arguments: an input string and a shift factor
+# Returns the modified string--the input string is unchanged
+# Uses the String#tr method
+# The #tr method will do a straighforward wrap for integer and alpha
+#  characters
+
+def caesar_cipher(clear_text, shift_factor)
+  ciphered = clear_text.dup
+  shift_factor.times { ciphered.tr!('0-9a-zA-Z','1-90b-zbB-ZA') }
+  ciphered
+end
