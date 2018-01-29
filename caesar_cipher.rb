c0887889581f251acc3875bb1a6a895bf4f8d0f2
xon-xoff <dantesraven@yahoo.com>
2018-01-29 05:31:09 -0500

    Update caesar_cipher.rb to correct error -- line 11

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,6 +8,6 @@
 
 def caesar_cipher(clear_text, shift_factor)
   ciphered = clear_text.dup
-  shift_factor.times { ciphered.tr!('0-9a-zA-Z','1-90b-zbB-ZA') }
+  shift_factor.times { ciphered.tr!('0-9a-zA-Z','1-90b-zaB-ZA') }
   ciphered
 end
