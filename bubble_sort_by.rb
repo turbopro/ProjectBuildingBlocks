aa7a1a562b31ff5ba1c9f33fa9f3153465ca7294
xon-xoff <dantesraven@yahoo.com>
2018-02-14 11:14:58 -0500

    Update bubble_sort.rb to include function 'bubble-sort_by'; add bubble-sort_by.rb

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,35 +1,3 @@
-#####	Function: bubble_sort
-
-# Takes one argument: an input array of integers
-# Returns the sorted array itself
-# Based on the bubble sort algorithm discussed on the Wikipedia
-# page here --> https://en.wikipedia.org/wiki/Bubble_sort
-#
-# Use the splat (*) operator to create an array from a range
-# Use the begin / end until loop
-
-def bubble_sort arr
-  
-  return arr if arr.length <= 1
-  
-  len = arr.length
-  begin 
-    swap_idx = 0
-    [*1...len].each do |idx|
-      if arr[idx-1] > arr[idx]
-        arr[idx], arr[idx-1] = arr[idx-1], arr[idx]
-        swap_idx = idx
-      end
-    end
-    len = swap_idx
-  end until len == 0  
-  arr
-end
-
-bubble_sort(Array.new(10)    { rand(1..51)     })
-bubble_sort(Array.new(10000) { rand(1..100000) }); nil
-
-
 #####	Function: bubble_sort_by
 
 # Takes one argument: an input array of strings to be sorted
@@ -74,9 +42,11 @@ end
 bubble_sort_by(%W(hi hello hey)) { |left, right| left.length - right.length }
 
 bubble_sort_by(["I"]) { |left, right| left.length - right.length }
+
 bubble_sort_by([]) { |left, right| left.length - right.length }
 
 bubble_sort_by([])
+
 bubble_sort_by([]) {}
 bubble_sort_by([]) { "" }
 
